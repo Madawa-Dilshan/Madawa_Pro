338f7c9c96be6798a905c11449d98cfa0860262b
Madawa-Dilshan <madawadilshan2018@gmail.com>
2022-01-03 16:03:46 +0530

    Initial commit

---
+++ MadawaPro1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MadawaPro1.MvcApplication" Language="C#" %>
---
+++ MadawaPro1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MadawaPro1.Startup))]
+namespace MadawaPro1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
